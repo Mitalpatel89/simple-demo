bc0b40fea65ad65c0dfd69aa267a94d3d4ee61d8
Mitalpatel89 <mitalpatel150589@gmail.com>
2025-07-04 12:36:01 +0530

    Add files via upload

---
+++ script1.py
@@ -0,0 +1,14 @@
+#Mathematical operation
+
+a=int(input('enter the first number: '))
+b=int(input('enter the second number: '))
+c=int(a+b)
+d=int(a-b)
+e=int(a*b)
+f=int (a/b)
+print('addition: ',c)
+print('subtraction: ',d)
+print('multiplication: ',e)
+print('division: ',f)
+
+
---
+++ script2.py
@@ -0,0 +1,6 @@
+#Greeting program
+
+a=str(input('Enter your first name: '))
+b=str(input('Enter your last name: '))
+c=str(a + b)
+print("Hello,"+"",c +" !"+" welcome "+"to "+"the "+"python "+"program. ")
